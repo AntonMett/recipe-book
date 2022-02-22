f9fdd5e3393bc47eaafd761db6e9b1291486335b
Anton <antonmett@gmail.com>
2022-02-22 19:51:23 +0200

    working on recipes

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Burger', 'This is delicious burger!', 'https://media-cdn.tripadvisor.com/media/photo-s/07/6f/a8/1d/yum-yum.jpg')
+  ];
+
 
   constructor() { }
 
